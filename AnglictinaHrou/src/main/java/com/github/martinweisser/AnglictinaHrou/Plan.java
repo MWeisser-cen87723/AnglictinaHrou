7880c0e2adb6c75a1bb2adfc9bfc58866b70acb4
martinweisser <martinweisser@192.168.0.10>
2018-04-06 22:12:52 +0200

    Vytvoření slov v metodě novyPlan

--- AnglictinaHrou/src/main/java/com/github/martinweisser/AnglictinaHrou/Plan.java
+++ AnglictinaHrou/src/main/java/com/github/martinweisser/AnglictinaHrou/Plan.java
@@ -12,13 +12,19 @@ public class Plan {
 	 */
 	public Plan( ) {
 		
+		novyPlan();
 	}
 	
 	/**
 	 * Metoda pro založení plánu
 	 */
 	public void novyPlan() {
-		
+		Slovo slovo1 = new Slovo ("medvěd", "bear");
+		Slovo slovo2 = new Slovo ("dům", "house");
+		Slovo slovo3 = new Slovo ("květina", "flower");
+		Slovo slovo4 = new Slovo ("opice", "monkey");
+		Slovo slovo5 = new Slovo ("auto", "car");
+		Slovo slovo6 = new Slovo ("okno", "window");
 	}
 
 }
