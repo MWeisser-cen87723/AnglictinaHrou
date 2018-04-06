b5a3423f94edf2bf33a92b71ea6b6676324e80ed
martinweisser <martinweisser@mw-macbook-pro>
2018-04-06 22:05:32 +0200

    Založení třídy Plan, vytvoření základních metod v třídě Slovo

---
+++ AnglictinaHrou/src/main/java/com/github/martinweisser/AnglictinaHrou/Plan.java
@@ -0,0 +1,24 @@
+package com.github.martinweisser.AnglictinaHrou;
+
+/** Třída Plan
+ * 
+ * @author martinweisser
+ *
+ */
+public class Plan {
+	
+	/**
+	 * Konstruktor
+	 */
+	public Plan( ) {
+		
+	}
+	
+	/**
+	 * Metoda pro založení plánu
+	 */
+	public void novyPlan() {
+		
+	}
+
+}
