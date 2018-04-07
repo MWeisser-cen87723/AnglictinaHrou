6650106a1df0c0afd2a3f6719ad5b8096177c525
martinweisser <martinweisser@192.168.0.10>
2018-04-07 23:55:11 +0200

    Controller - implementace metod tipuj, noveOkno, zobrazeniObrazku, prevedeni generovani cisel do samostatne metody

--- AnglictinaHrou/src/main/java/com/github/martinweisser/AnglictinaHrou/Controller.java
+++ AnglictinaHrou/src/main/java/com/github/martinweisser/AnglictinaHrou/Controller.java
@@ -3,11 +3,16 @@ package com.github.martinweisser.AnglictinaHrou;
 import java.util.Random;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class Controller {
 	
@@ -25,7 +30,7 @@ public class Controller {
 	@FXML private TextFlow monkeyText;
 	@FXML private TextFlow flowerText;
 	
-	@FXML private TextArea tipText;
+	@FXML private TextField tipText;
 	@FXML private Label tipLbl;
 	@FXML private VBox tipWindow;
 	@FXML private VBox statistikaWindow;
@@ -34,6 +39,8 @@ public class Controller {
 	
 	private int spatna;
 	private int spravna;
+	private int nahodne;
+	private boolean odpoved;
 
 	
 	@FXML public void bear() {
@@ -69,16 +76,11 @@ public class Controller {
 		monkeyText.setVisible(false);
 	}
 	
+	
 	@FXML public void hadej() {
-		Random cislo = new Random();
-        int  nahodne = cislo.nextInt(6) + 1;
-        
-        bear.setVisible(false);
-		window.setVisible(false);
-		car.setVisible(false);
-		flower.setVisible(false);
-		house.setVisible(false);
-		monkey.setVisible(false);
+		
+		nahodnaCisla();
+        zobrazeniObrazku();
 		
 		tipWindow.setVisible(true);
 		tipText.setEditable(true);
@@ -105,6 +107,95 @@ public class Controller {
 	}
         
         @FXML public void tipuj() {
+        		
+        		String vstupniText = tipText.getText();
+        		odpoved = false;
+        		
+        		if (nahodne == 1 && vstupniText.equals("bear")) {
+        			spravna ++;
+        			odpoved = true;
+        		}
+        		else if (nahodne == 2 && vstupniText.equals("window")) {
+        			spravna ++;
+        			odpoved = true;
+        		}
+        		else if (nahodne == 3 && vstupniText.equals("car")) {
+        			spravna ++;
+        			odpoved = true;
+        		}
+        		else if (nahodne == 4 && vstupniText.equals("flower")) {
+        			spravna ++;
+        			odpoved = true;
+        		}
+        		else if (nahodne == 5 && vstupniText.equals("house")) {
+        			spravna ++;
+        			odpoved = true;
+        		}
+        		else if (nahodne == 6 && vstupniText.equals("monkey")) {
+        			spravna ++;
+        			odpoved = true;
+        		} else {
+        			spatna ++;
+        			odpoved = false;
+        		}
+        		
+        		statistika.setText("Správně: " + spravna + "     Špatně: " + spatna);
+        		tipText.setText("");
+        		noveOkno();
+        		zobrazeniObrazku();
+        		
+        }
+        
+        public void nahodnaCisla() {
+        		Random cislo = new Random();
+            nahodne = cislo.nextInt(6) + 1;
+        }
+        
+        public void noveOkno() {
+        		
+        		StackPane newScene = new StackPane();
+        		
+        		if (odpoved) {
+        			Label about = new Label("Výborně! Uhodl jsi!");
+        			newScene.getChildren().add(about);
+        		} else {
+        			Label about = new Label("Bohužel, odpověděl jsi nesprávně!");
+        			newScene.getChildren().add(about);
+        		}
+
+            Scene scene = new Scene(newScene, 300, 100);
+            Stage newWindow = new Stage();
+
+            newWindow.setTitle("Tvá odpověď!");
+            newWindow.setScene(scene);
+            newWindow.setAlwaysOnTop(true);
+            newWindow.centerOnScreen();
+            newWindow.initModality(Modality.WINDOW_MODAL);
+            newWindow.setX(500);
+            newWindow.setY(300);
+
+            newWindow.show();
+        }
+        
+        public void zobrazeniObrazku() {
+        		if (!bear.isVisible() || !window.isVisible() || !flower.isVisible() || !car.isVisible() || !monkey.isVisible() || !house.isVisible()) {
+        			
+        			bear.setVisible(true);
+        			window.setVisible(true);
+        			car.setVisible(true);
+        			flower.setVisible(true);
+        			house.setVisible(true);
+        			monkey.setVisible(true);
+        			
+        		} else {
+        			
+        			bear.setVisible(false);
+        			window.setVisible(false);
+        			car.setVisible(false);
+        			flower.setVisible(false);
+        			house.setVisible(false);
+        			monkey.setVisible(false);
+        		}
         }
         
 	}
