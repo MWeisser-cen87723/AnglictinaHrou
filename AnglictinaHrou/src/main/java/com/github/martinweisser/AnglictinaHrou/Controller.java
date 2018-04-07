773023de270215a80ba3d1adeca6965af512e432
martinweisser <martinweisser@192.168.0.10>
2018-04-07 15:57:18 +0200

    Logika předána Controlleru a souboru fxml

--- AnglictinaHrou/src/main/java/com/github/martinweisser/AnglictinaHrou/Controller.java
+++ AnglictinaHrou/src/main/java/com/github/martinweisser/AnglictinaHrou/Controller.java
@@ -14,6 +14,11 @@ public class Controller {
 	@FXML private ImageView window;
 	
 	@FXML private TextFlow bearText;
+	@FXML private TextFlow windowText;
+	@FXML private TextFlow houseText;
+	@FXML private TextFlow carText;
+	@FXML private TextFlow monkeyText;
+	@FXML private TextFlow flowerText;
 	
 	private int spatna;
 	private int spravna;
@@ -23,8 +28,33 @@ public class Controller {
 		bearText.setVisible(true);
 	}
 	
+	@FXML public void window() {
+		windowText.setVisible(true);
+	}
+	
+	@FXML public void car() {
+		carText.setVisible(true);
+	}
+	
+	@FXML public void house() {
+		houseText.setVisible(true);
+	}
+	
+	@FXML public void flower() {
+		flowerText.setVisible(true);
+	}
+	
+	@FXML public void monkey() {
+		monkeyText.setVisible(true);
+	}
+	
 	@FXML public void viditelnostObrazku() {
 		bearText.setVisible(false);
+		windowText.setVisible(false);
+		carText.setVisible(false);
+		flowerText.setVisible(false);
+		houseText.setVisible(false);
+		monkeyText.setVisible(false);
 	}
 
 }
